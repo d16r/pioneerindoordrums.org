0de27bd28e85858ea4dea5ba8786396ea22e882d
Dustin Shahidehpour <dshahidehpour@gmail.com>
2014-06-09 20:57:29 -0500

    Markup is now working on the page.

--- app/scripts/controllers/faqs.js
+++ app/scripts/controllers/faqs.js
@@ -4,6 +4,8 @@ angular.module('pioApp').controller('FAQCtrl', function ($scope, FAQ) {
     $scope.faqs = FAQ.all;
     $scope.submitNewFAQ = function() {
         FAQ.create($scope.faq)
+        $scope.faq.question = ''
+        $scope.faq.answer = ''
     }
     $scope.deleteQuestion = function(id) {
         FAQ.remove(id);
