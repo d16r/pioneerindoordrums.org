01048e97010a365b8f15807a6bf9086616c01fbf
Dustin Shahidehpour <dshahidehpour@gmail.com>
2014-06-14 14:24:50 -0500

    removed unused values for models.

--- app/scripts/controllers/faqs.js
+++ app/scripts/controllers/faqs.js
@@ -11,30 +11,21 @@ angular.module('pioApp').controller('FAQCtrl', function ($scope, FAQ, Page) {
     
     $scope.setValuesForCreate = function() {
         $scope.isUpdate = false;
-        $scope.faq = '';
-        $scope.faqId = '';
     }
     
-    $scope.setValuesForUpdate = function(id, faq) {
+    $scope.setValuesForUpdate = function() {
         $scope.isUpdate = true;
-        $scope.faq = faq;
-        $scope.faqId = id;
     }
     
     $scope.deleteQuestion = function() {
         FAQ.remove($scope.deleteId);
-        $scope.deleteId = '';
     }
     
     $scope.updateFAQ = function() {
         FAQ.update($scope.faqId, $scope.faq);
-        $scope.faq = '';
-        $scope.faqId = '';
     }
     
     $scope.submitNewFAQ = function() {
         FAQ.create($scope.faq);
-        $scope.faq = '';
-        $scope.faqId = '';
     }
 });
