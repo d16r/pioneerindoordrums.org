597e6c84ea53a7d96fe19f59dd6367d7816d6473
Dustin Shahidehpour <dshahidehpour@gmail.com>
2014-06-14 10:20:10 -0500

    Added response to user when you send an email.

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -4,13 +4,17 @@ angular.module('pioApp').controller('ContactCtrl', function ($scope, $http, Page
     Page.setTitle('Pioneer Indoor - Contact');
 
     $scope.sendContactForm = function() {
-        alert($scope.contact.firstName);
-        $http.post('http://0.0.0.0:5000/email/contact', $scope.contact).
+        $scope.message = 'Thanks! Your message was sent. We will get back to you ASAP!';
+        $scope.success = true;
+        /*$http.post('http://0.0.0.0:5000/email/contact', $scope.contact).
           success(function(data, status, headers, config) {
               $scope.message = 'Thanks! Your message was sent, and we will get back to you soon!'
+              $scope.success = true;
           }).
           error(function(data, status, headers, config) {
+              $scope.error = true;
               $scope.message = 'Unfortunately, something went wrong with our servers. Feel free to send an email to dshahidehpour@gmail.com';
           });
+        */
     }
 });
