2b54674e861d3fd23789f58a6c76408ec998d95c
Dustin Shahidehpour <dshahidehpour@gmail.com>
2014-06-29 15:37:13 -0500

    removed a lot views.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,8 +6,7 @@ var app = angular.module('pioApp', ['ngCookies',
                                     'ngRoute',
                                     'firebase',
                                     'angulartics',
-                                    'angulartics.google.analytics',
-                                    'ui.bootstrap'
+                                    'angulartics.google.analytics'
                                     ]);
 
 app.config(function ($routeProvider, $httpProvider) {
@@ -18,28 +17,8 @@ app.config(function ($routeProvider, $httpProvider) {
       .when('/', {
         templateUrl: 'views/main.html'
       })
-      .when('/faqs', {
-        templateUrl: 'views/faqs.html'
-      })
       .when('/admin', {
         templateUrl: 'views/admin.html'
-      })
-      .when('/history', {
-        templateUrl: 'views/history.html'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html'
-      })
-      .when('/staff', {
-        templateUrl: 'views/staff.html'
-      })
-      .when('/contact', {
-        templateUrl: 'views/contact.html',
-        controller: 'ContactCtrl'
-      })
-      .when('/auditions', {
-        templateUrl: 'views/auditions.html',
-        controller: 'AuditionCtrl'
       })
       .when('/login', {
         templateUrl: 'views/login.html',
